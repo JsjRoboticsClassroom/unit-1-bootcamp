a4d68e4d505344be8f58e4ce749f8c8bb112a0e7
MinaMeltem <meltem@gmail.com>
2016-12-25 20:33:19 -0500

    Method Exercises

--- src/Methods.java
+++ src/Methods.java
@@ -1,9 +1,97 @@
+import java.util.Random;
+
 /**
- * Created by alexandraqin on 4/14/15.
+
+
+ 6. Random
+
+ Write a method called random that takes 2 ints as parameters and returns a random int within that range.
+
+ ex: random(1, 100) returns a random int between 1 and 100
+
  */
 public class Methods {
+    public static void main(String args[]) {
 
-  public static void main (String args[]) {
+        //ans1
+        System.out.println(calculateSquare(2));
 
-  }
+        //ans2
+        System.out.println(calculateSquareRoot(4));
+
+        //ans3
+        System.out.println(toLowerCase("Hello, World!"));
+
+        //ans4
+        System.out.println(isMultiple(5,101));
+
+        //ans5
+        prettyInteger(4);
+
+        //ans6
+        System.out.println(random(1,100));
+
+
+
+
+    }
+
+    //question 1
+    public static int calculateSquare(int edgeLength) {
+        return edgeLength * edgeLength;
+    }
+
+    //question2
+    public static int calculateSquareRoot(int number) {
+        return number * number;
+    }
+
+    //question3
+    public static String toLowerCase(String myString) {
+        return myString.toLowerCase();
+
+    }
+
+    //question 4
+    public static boolean isMultiple(int num1, int num2) {
+        if(num2>num1) {
+            if (num2 % num1 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    //question5
+
+    public static void prettyInteger( int myInt){
+        String stars =" ";
+        for(int i = 0; i<myInt; i++){
+           stars += "*";
+        }
+        System.out.println(stars+" "+myInt+stars);
+    }
+
+    //question6
+
+    public static int random (int minValue, int maxValue){
+        Random rand = new Random();
+        if (!(maxValue > minValue)){
+            int temp = maxValue;
+            maxValue =minValue;
+            minValue =temp;
+        }
+        return (rand.nextInt(maxValue - minValue +1) + minValue);
+    }
+
 }
+
+
+
+
+
+
+
+
+
